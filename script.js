230fba1761c80d8237dec03c7d2080b7bf99f55e
mca-programmer <freelancermca52@gmail.com>
2025-09-09 15:53:07 +0600

    Updata navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+//  navbar mobile res
+ const menuBtn = document.getElementById("menuBtn");
+    const mobileMenu = document.getElementById("mobileMenu");
+
+    menuBtn.addEventListener("click", () => {
+      mobileMenu.classList.toggle("hidden");
+    });
